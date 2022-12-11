4bf275c6baa709ee557f2491debe4cdaad025ad6
Akshay Verma <topensite@gmail.com>
2022-12-11 12:50:44 +0530

    final

--- express and rest intro/app.js
+++ express and rest intro/app.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express()
 
-app.use((req,res,next)=>{
+app.use('/hello',(req,res,next)=>{
     console.log(hi)
+    next()
 })
+
+app.use('/',(req,res,next)=>{
+    console.log('hello')
+})
+
+
+app.listen(3000)
+
