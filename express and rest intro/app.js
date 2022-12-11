601799dd338ef054c50868f469c9d4ed95ce65e2
Akshay Verma <topensite@gmail.com>
2022-12-11 12:42:16 +0530

    Added express

--- express and rest intro/app.js
+++ express and rest intro/app.js
@@ -1,26 +1,7 @@
-const http = require('http')
+const express = require('express')
 
-const fs = require('fs');
+const app = express()
 
-const server = http.createServer((req,res)=>{
-   if(req.url === '/'){
-    res.setHeader('Content-Type','text/html');
-    res.write("<form action='/products' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>");
-   }
-   else if(req.url === '/products' && req.method === 'POST'){
-    const body = [];
-    req.on('data',(chunk)=>{
-        body.push(chunk);
-        console.log(body);
-    });
-
-    req.on('end',()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        console.log(parsedBody);
-    });
-    res.write("<h1>Laptop</h1>");
-   }
-    res.end();
-});
-
-server.listen(3000);
+app.use((req,res,next)=>{
+    console.log(hi)
+})
